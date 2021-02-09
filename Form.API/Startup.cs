156ce8996380ee658a6c102ef2e5caea91459353
Luis de la Rosa Minaya <luisdelarosaminaya@gmail.com>
2021-02-09 09:58:55 -0400

    Configure Swashbuckle

--- Form.API/Startup.cs
+++ Form.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 
 using Microsoft.EntityFrameworkCore;
 using Form.API.Models.Context;
+using Microsoft.OpenApi.Models;
 
 namespace Form.API
 {
@@ -34,6 +35,20 @@ namespace Form.API
                 opts.UseSqlServer(Configuration["Db:SqlServerConnectionString"]);
                 opts.EnableSensitiveDataLogging();
             }, ServiceLifetime.Transient);
+
+            services.AddSwaggerGen(opts => {
+                opts.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "User form API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "luixcode",
+                        Url = new Uri("https://github.com/luixcode")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,9 +61,17 @@ namespace Form.API
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "User form API");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthorization();            
 
             app.UseEndpoints(endpoints =>
             {
